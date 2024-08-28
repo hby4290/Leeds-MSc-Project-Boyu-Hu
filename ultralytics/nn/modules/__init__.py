a1eae573a122c55775cbc24b3f40dafd04158743
hby4290 <huboyu_2000@163.com>
2024-08-28 11:18:25 +0100

    Update __init__.py

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -1,22 +1,3 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-"""
-Ultralytics modules.
-
-Example:
-    Visualize a module with Netron.
-    ```python
-    from ultralytics.nn.modules import *
-    import torch
-    import os
-
-    x = torch.ones(1, 128, 40, 40)
-    m = Conv(128, 128)
-    f = f'{m._get_name()}.onnx'
-    torch.onnx.export(m, x, f)
-    os.system(f'onnxslim {f} {f} && open {f}')  # pip install onnxslim
-    ```
-"""
-
 from .block import (
     C1,
     C2,
