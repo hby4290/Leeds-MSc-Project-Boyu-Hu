90a7955aa73da87f36d8f5e0bdc23218c403997a
hby4290 <huboyu_2000@163.com>
2024-08-28 11:16:31 +0100

    Update MLLABlock.py

--- ultralytics/nn/Addmodules/MLLABlock.py
+++ ultralytics/nn/Addmodules/MLLABlock.py
@@ -1,26 +1,9 @@
-# --------------------------------------------------------
-# Swin Transformer
-# Copyright (c) 2021 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ze Liu
-# --------------------------------------------------------
-# Demystify Mamba in Vision: A Linear Attention Perspective
-# Modified by Dongchen Han
-# -----------------------------------------------------------------------
- 
 import torch
 import torch.nn as nn
  
 __all__ = ['C2fMLLABlock']
  
 def drop_path(x, drop_prob: float = 0., training: bool = False, scale_by_keep: bool = True):
-    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
-    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
-    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
-    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
-    'survival rate' as the argument.
-    """
     if drop_prob == 0. or not training:
         return x
     keep_prob = 1 - drop_prob
